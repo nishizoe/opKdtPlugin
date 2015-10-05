239dfcb05b50f1342bdd0d6bc9af2ccf0e50d320
Kimura Youichi <kim.upsilon@bucyou.net>
2015-10-05 16:13:43 +0900

    メンバー切り替えガジェットのデフォルト値をログイン状態に合わせる

--- lib/form/opKdtMemberSelectForm.class.php
+++ lib/form/opKdtMemberSelectForm.class.php
@@ -4,7 +4,10 @@ class opKdtMemberSelectForm extends sfForm
 {
   public function configure()
   {
-    $this->setWidget('id', new sfWidgetFormDoctrineChoice(array('model' => 'Member', 'add_empty' => '未ログイン')));
+    $currentUser = sfContext::getInstance()->getUser();
+    $memberId = $currentUser->isSNSMember() ? $currentUser->getMemberId() : '';
+
+    $this->setWidget('id', new sfWidgetFormDoctrineChoice(array('model' => 'Member', 'add_empty' => '未ログイン', 'default' => $memberId)));
     $this->setValidator('id', new sfValidatorDoctrineChoice(array('model' => 'Member', 'required' => false)));
 
     $this->widgetSchema->setNameFormat('member_select[%s]');
